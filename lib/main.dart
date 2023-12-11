9ba003401aee86f4252c007a61178b8110473dd1
AbhishekBhosale02 <avbhosale02@gmail.com>
2023-12-11 19:40:00 +0530

    cakibration_check for Local Audio file (with many errors)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+// import 'package:sih_23_audiometer/pages/calibration_check.dart';
 import 'package:sih_23_audiometer/pages/home_page.dart';
 import 'package:sih_23_audiometer/pages/student_dashboard.dart';
 import 'package:sih_23_audiometer/pages/student_login.dart';
 import 'package:sih_23_audiometer/pages/student_signup.dart';
 import 'package:sih_23_audiometer/utils/routes.dart';
 import 'package:sih_23_audiometer/widgets/themes.dart';
-
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,12 +24,13 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.backgroundNoise,
+      initialRoute: MyRoutes.calibration_check,
       routes: {
         MyRoutes.homeRoute: (context) => const HomePage(),
         MyRoutes.studentLoginRoute: (context) => const student_login(),
         MyRoutes.studentDashboard: (context) => const StudentDashboard(),
         MyRoutes.studentSignupRoute: (context) => const StudentSignup(),
+        // MyRoutes.calibration_check:(context)=> Calibration(),
         // MyRoutes.backgroundNoise: (context) => const BackgroundNoise(),
       },
     ); 
