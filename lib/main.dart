e7f0a74c5f54989b0395adc631101861935c2b02
MohiniMehta <mohinimehta12@gmail.com>
2023-12-12 11:09:26 +0530

    headphone connectivity done completely

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:sih_23_audiometer/pages/background_noise.dart';
+import 'package:sih_23_audiometer/pages/headset.dart';
 import 'package:sih_23_audiometer/pages/home_page.dart';
 import 'package:sih_23_audiometer/pages/student_dashboard.dart';
 import 'package:sih_23_audiometer/pages/student_login.dart';
@@ -24,14 +25,15 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.backgroundNoise,
+      initialRoute: MyRoutes.headset,
       routes: {
         MyRoutes.homeRoute: (context) => const HomePage(),
         MyRoutes.studentLoginRoute: (context) => const student_login(),
         MyRoutes.studentDashboard: (context) => const StudentDashboard(),
         MyRoutes.studentSignupRoute: (context) => const StudentSignup(),
         MyRoutes.backgroundNoise: (context) => const BackgroundNoise(),
+        MyRoutes.headset: (context) => const Headset(),
       },
-    ); 
+    );
   }
 }
