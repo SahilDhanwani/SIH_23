104d86acb4fc5abafb639f9814402bc73b0d55ce
Vaishnavi <vaishnavinaik0022@gmail.com>
2023-12-14 10:50:59 +0530

    minor changes

--- lib/pages/Calibration.dart
+++ lib/pages/Calibration.dart
@@ -1,3 +1,7 @@
+// ignore: duplicate_ignore
+// ignore: file_names
+// ignore_for_file: file_names
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
